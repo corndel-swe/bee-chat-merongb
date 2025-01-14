9020cd0cbcdb78e4fdbb1ab7250ef0e907389763
Meron G <merong@Merons-MacBook-Air.local>
2025-01-14 01:31:58 +0000

    added functionality that logs user connectivity to all users in the session pool

--- server/src/main/java/config/MyWebSocketHandler.java
+++ server/src/main/java/config/MyWebSocketHandler.java
@@ -4,13 +4,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class MyWebSocketHandler extends TextWebSocketHandler {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
         System.out.println("Connected:" + session.getId());
+        Message serverResponse = new Message(session.getId() + " has joined the chat");
+        String jsonResponse = objectMapper.writeValueAsString(serverResponse);
+
+
+        broadcastMessage(jsonResponse);
     }
 
     @Override
@@ -18,20 +31,26 @@ public class MyWebSocketHandler extends TextWebSocketHandler {
         String messageContent = message.getPayload().toString();
         System.out.println("Received Message: " + messageContent);
 
-        Message serverResponse = new Message("Hello from the server");
-
-        String jsonResponse = objectMapper.writeValueAsString(serverResponse);
-
-        session.sendMessage(new TextMessage(jsonResponse));
-
         try {
 
             Message messageObject = objectMapper.readValue(messageContent, Message.class );
-            System.out.println(messageObject.getRecipientId());
-            System.out.println(messageObject.getContent());
+            Message serverResponse = new Message(messageObject.getRecipientId().toString() + messageObject.getContent());
+
+
+            String jsonResponse = objectMapper.writeValueAsString(serverResponse);
+            session.sendMessage(new TextMessage(jsonResponse));
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+
+    private void broadcastMessage(String message) throws IOException {
+        for (WebSocketSession session : sessions) {
+            if (session.isOpen()) {
+                session.sendMessage(new TextMessage(message));
+            }
         }
     }
 
